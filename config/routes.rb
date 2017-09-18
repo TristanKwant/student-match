4a1a31c5fdc0121bcc16e1c016b64aa4e56d39d3
TristanKwant <tristankwant@gmail.com>
2017-09-18 16:40:14 +0200

    add match model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+
   # get 'pages/home'
   root to: 'pages#home'
 
   devise_for :users
-  resources :users
+  resources :users do
+    resources :matches 
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170918095057) do
+ActiveRecord::Schema.define(version: 20170918125356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "profiles", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.boolean "admin", default: false
-    t.bigint "user_id"
+  create_table "matches", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_matches_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,5 +40,5 @@ ActiveRecord::Schema.define(version: 20170918095057) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "profiles", "users"
+  add_foreign_key "matches", "users"
 end
