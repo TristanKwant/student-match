ac70404a39da401e251b4e42cc23cb1b8dc125cb
TristanKwant <tristankwant@gmail.com>
2017-09-19 15:53:50 +0200

    add day model

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,9 @@ user1 = User.create!(email: "tristan@admin.com", password: "123456", admin: true
 user2 = User.create!(email: "marloes@admin.com", password: "123456", admin: true)
 user3 = User.create!(email: "tristan@student.com", password: "123456")
 user4 = User.create!(email: "marloes@student.com", password: "123456")
+
+
+day1 = Day.create!(datematch: "19 sept 2017")
+day2 = Day.create!(datematch: "20 sept 2017")
+day3 = Day.create!(datematch: "21 sept 2017")
+day4 = Day.create!(datematch: "22 sept 2017")
