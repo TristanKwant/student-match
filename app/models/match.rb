043b929696eae6702b627cf6798730fbddc268c7
loezzje <m.demmendaal@hotmail.com>
2017-09-21 17:22:25 +0200

    model match update

--- app/models/match.rb
+++ app/models/match.rb
@@ -6,10 +6,10 @@ class Match < ApplicationRecord
 
 
 def get_match_list
-users = User.all.ids.shuffle
+users = User.all.where(admin: false).ids.shuffle
 users.push(nil) if users.size.odd?
 get_match_list = (1...users.size).map do |r|
-    u=users.dup
+    u = users.dup
     (0...(users.size/2)).map do |_|
       [u.shift, u.delete_at(-(r % u.size + (r >= u.size * 2 ? 1 : 0)))]
     end
