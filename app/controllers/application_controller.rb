765586b841abc5188c6bfb9f33a97827d51cb196
TristanKwant <tristankwant@gmail.com>
2017-09-22 14:07:47 +0200

    add features test for day

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
   if resource.admin
-    users_path
+    days_path
   else
-    root_path
+    days_path
   end
   end
 
