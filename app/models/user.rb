563f45e643896ebe7315c12183fb8c9342ca4699
loezzje <m.demmendaal@hotmail.com>
2017-09-22 10:44:52 +0200

    add profile and update view and update navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,16 @@ class User < ApplicationRecord
   has_many :matches
   has_one :profile
 
-
-
-
   def sort
     order(id: :desc)
   end
 
+  def has_profile?
+    profile.present? && profile.persisted?
+  end
 
-
+  def full_name
+    profile.full_name
+  end
 
 end
